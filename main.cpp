89d7b120500133cfc3826539e10625f19986009a
livandan <livandan@sjtu.edu.cn>
2025-05-31 00:04:19 +0800

    Drag main.cpp here.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,35 @@
 #include "b_plus_tree.h"
 
 int main() {
-  sjtu::bpt<int *> bpt("map_info.txt", "data.txt");
-  int group[2];
-  group[0] = 0;
-  group[1] = 1;
-  bpt.Insert("first", group);
-  bpt.Insert("second", "2");
-  auto ans = bpt.Find("first");
-  for (const auto &it : ans) {
-    std::cout << it << std::endl;
+  sjtu::bpt<int> bpt("map_file.txt", "data_file.txt");
+
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; ++i) {
+    std::string command;
+    std::cin >> command;
+    if (command == "insert") {
+      std::string index;
+      int value;
+      std::cin >> index >> value;
+      bpt.Insert(index, value);
+    } else if (command == "delete") {
+      std::string index;
+      int value;
+      std::cin >> index >> value;
+      bpt.Delete(index, value);
+    } else if (command == "find") {
+      std::string index;
+      std::cin >> index;
+      const auto ans = bpt.Find(index);
+      for (const auto it : ans) {
+        std::cout << it << ' ';
+      }
+      if (ans.empty()) {
+        std::cout << "null";
+      }
+      std::cout << '\n';
+    }
   }
-  bpt.Delete("first", "1");
-  ans = bpt.Find("first");
-  for (const auto &it : ans) {
-    std::cout << it << std::endl;
-  }
-  std::cout << bpt.Size();
   return 0;
 }
